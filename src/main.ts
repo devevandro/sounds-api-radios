d775f9cc6eac8e5c3868ef02f717ca74f839c9bf
devevandro <dev.evandro@gmail.com>
2024-11-02 13:27:24 -0300

    refactor: changemain config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './AppModule';
@@ -11,24 +10,7 @@ import { connectToDataBase } from './providers/mongo/connections';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  if (ENVIRONMENTS.NODE_ENV !== 'test') {
-    const config = new DocumentBuilder()
-      .setTitle('Nestjs Template Documentation')
-      .setDescription('The Nestjs API description')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config, {
-      deepScanRoutes: true,
-    });
-    SwaggerModule.setup('api', app, document);
-  }
-
   await connectToDataBase();
   await app.listen(ENVIRONMENTS.PORT);
-
-  return app;
 }
-
-export const app = bootstrap();
+bootstrap();
